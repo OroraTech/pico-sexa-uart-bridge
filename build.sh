9c73a282b15d88995582ceab4fed30585de4606a
Chris Burton <Chris@8086.net>
2025-03-25 10:06:09 +0000

    Update to build for Pico and Pico2

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-BUILD_DIR=build
+BUILD_DIR=build-pico
+BUILD_DIR2=build-pico2
+
 PICO_SDK_DIR=pico-sdk
 
 main () {
@@ -12,6 +14,11 @@ main () {
 	mkdir -p $BUILD_DIR
 	cmake -B $BUILD_DIR
 	make -C $BUILD_DIR
+
+	mkdir -p $BUILD_DIR2
+	cmake -B $BUILD_DIR2 -DPICO_BOARD=pico2
+	make -C $BUILD_DIR2
+
 }
 
 main $@
